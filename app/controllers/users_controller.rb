837b3ecec804cef4aaf0a228819f5228d330c25d
atsushi59 <atsushiyamada59@gmail.com>
2024-03-22 21:00:43 +0900

    yml

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash.now[:succsess] = 'ユーザーを登録しました'
+      redirect_to user_path
+    else
+      flash.now[:danger] = 'ユーザーの登録に失敗しました'
+      rendew :new, status: :unprocessable_entity
+    end  
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name,:email, :password, :password_digest, :name, :avatar)
+  end  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ApplicationRecord
+    authenticates_with_sorcery!
+
+    validates :user_name, presence: true, length: { maximum: 16 }
+    validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@\S+\.\S+\z/, message: "is not valid" }
+    validates :name, presence: true, length: { maximum: 8 }, uniqueness: true
+    validates :avatar, format: { with: /\.(png|jpg|jpeg)\z/i, message: "must be a valid image format" }
+    validates :password, length: { minimum: 8 }
+    validates :password_confirmation, presence: true, on: :create
+    validate :password_complexity
+
+
+    private
+
+    def password_complexity
+        return if password.blank? || password =~ /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+}{"':;?/>.<,])[A-Za-z\d!@#$%^&*()_+}{"':;?/>.<,]{8,}\z/
+    
+        errors.add :password, "must include at least one lowercase letter, one uppercase letter, one digit, and one special character"
+    end
+
+
 end
+
+
