e84233f1e58533d31aa4f4d647df29f01a94a20a
atsushi59 <atsushiyamada59@gmail.com>
2024-03-27 11:59:45 +0900

    test2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+  
   get "up" => "rails/health#show", as: :rails_health_check
   root "static_pages#top"
   resources :users
+  resources :tasks
   get '/styles.css', to: 'static#styles'
 end
