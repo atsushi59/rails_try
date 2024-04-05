0a6cd86fa01a04cf2888737cf46b36dfae5652b7
atsushi59 <atsushiyamada59@gmail.com>
2024-04-06 00:24:40 +0900

    Google Places API実装

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,25 @@
 class StaticPagesController < ApplicationController
-end
+    def index
+      api_key = ENV['GOOGLE_PLACES_API_KEY']
+      google_places_service = GooglePlacesService.new(api_key)
+  
+      query = params[:query] || 'デフォルトの検索キーワード'  # フォームからの入力またはデフォルト値
+      search_response = google_places_service.search_places(query)
+      if search_response.parsed_response['candidates'].any?
+        first_result = search_response.parsed_response['candidates'].first
+        place_id = first_result['place_id']
+  
+        details_response = google_places_service.get_place_details(place_id)
+        @place_details = details_response.parsed_response['result']
+  
+        if @place_details['photos']
+          photo_reference = @place_details['photos'].first['photo_reference']
+          @photo_url = google_places_service.get_photo(photo_reference)
+        end
+      else
+        @place_details = nil
+        @photo_url = nil
+      end
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   
   get "up" => "rails/health#show", as: :rails_health_check
-  root "static_pages#top"
+  root "static_pages#index"
   resources :users
   resources :tasks
   get '/styles.css', to: 'static#styles'
