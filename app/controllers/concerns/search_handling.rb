0a5a57cfd67e84f00e10eff10d64cbcf91f220a3
atsushi59 <atsushiyamada59@gmail.com>
2024-04-25 16:10:42 +0900

    補足の追加

--- app/controllers/concerns/search_handling.rb
+++ app/controllers/concerns/search_handling.rb
@@ -7,10 +7,6 @@
 module SearchHandling
   extend ActiveSupport::Concern
 
-  included do
-    helper_method :generate_user_input, :generate_messages, :handle_response, :setup_session
-  end
-
   def setup_session
     params.each do |key, value|
       session[key] = value if %w[selected_transport address selected_time
@@ -30,7 +26,9 @@ module SearchHandling
   def generate_messages(user_input)
     [
       { role: 'system', content: 'You are a helpful assistant.' },
+      # roleはsystemであり、ChatGPTの動作モードや役割に関する指示を提供
       { role: 'user', content: user_input }
+      # roleはsystemであり、ChatGPTの動作モードや役割に関する指示を提供
     ]
   end
 
@@ -47,10 +45,16 @@ module SearchHandling
 
   def process_successful_response(response)
     @result = response.parsed_response
+    # response.parsed_response: 応答の内容を解析し、@result変数に格納
     message_content = @result['choices'].first['message']['content']
+    # 最初の選択肢(choices)のメッセージ内容を取得してmessage_contentに代入します。これは、ChatGPTが生成したテキスト内容
     places = message_content.split("\n").select { |line| line.match(/^\d+\./) }
+    # 取得したメッセージ内容を改行文字(\n)で分割し、各行を配列の要素として扱います
+    # その後、行の始めが数字で始まるもの（例：1. Place Name）を選択し、そのようなパターンに一致する行のみをplaces配列に格納
     @answer = places.join("\n")
+    # places配列の要素を改行で結合して@answerに代入
     session[:query] = @answer
+    # sessionでサーバーに一時的に保存
   end
 
   def process_error_response(response)
